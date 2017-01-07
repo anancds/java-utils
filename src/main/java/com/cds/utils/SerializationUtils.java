e8ded5f6b8ef096e4c587ff82cf67fbae7dcef4d
anancds <anancds@163.com>
2017-01-07 15:42:03 +0800

    Added SerializationUtils

---
+++ src/main/java/com/cds/utils/SerializationUtils.java
@@ -0,0 +1,61 @@
+package com.cds.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+/**
+ * <p></p>
+ *
+ * @author chendongsheng5 2017/1/7 15:37
+ * @version V1.0
+ * @modificationHistory =========================逻辑或功能性重大变更记录
+ * @modify by user: chendongsheng5 2017/1/7 15:37
+ * @modify by reason:{方法名}:{原因}
+ */
+public class SerializationUtils {
+	/**
+	 * Serialize the given object to a byte array.
+	 * @param object the object to serialize
+	 * @return an array of bytes representing the object in a portable fashion
+	 */
+	public static byte[] serialize(Object object) {
+		if (object == null) {
+			return null;
+		}
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(1024);
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(baos);
+			oos.writeObject(object);
+			oos.flush();
+		}
+		catch (IOException ex) {
+			throw new IllegalArgumentException("Failed to serialize object of type: " + object.getClass(), ex);
+		}
+		return baos.toByteArray();
+	}
+
+	/**
+	 * Deserialize the byte array into an object.
+	 * @param bytes a serialized object
+	 * @return the result of deserializing the bytes
+	 */
+	public static Object deserialize(byte[] bytes) {
+		if (bytes == null) {
+			return null;
+		}
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes));
+			return ois.readObject();
+		}
+		catch (IOException ex) {
+			throw new IllegalArgumentException("Failed to deserialize object", ex);
+		}
+		catch (ClassNotFoundException ex) {
+			throw new IllegalStateException("Failed to deserialize object type", ex);
+		}
+	}
+}
