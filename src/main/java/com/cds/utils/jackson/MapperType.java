09daf965a172f4c775d88d11750fb96bf7c60d13
anancds <anancds@163.com>
2017-01-17 16:01:17 +0800

    Added ObjectMapperFactory

---
+++ src/main/java/com/cds/utils/jackson/MapperType.java
@@ -0,0 +1,14 @@
+package com.cds.utils.jackson;
+
+/**
+ * <p>需要解析的对象的枚举</p>
+ *
+ * @author chendongsheng5 2017/1/5 16:56
+ * @version V1.0
+ * @modificationHistory =========================逻辑或功能性重大变更记录
+ * @modify by user: chendongsheng5 2017/1/5 16:56
+ * @modify by reason:{方法名}:{原因}
+ */
+public enum MapperType {
+	JSON, CSV, SMILE, XML, YAML
+}
