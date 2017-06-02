a010595a38f6fdca08ce991988caa1c2cc7399b5
anancds <anancds@163.com>
2017-06-02 14:52:55 +0800

    Added config

---
+++ scala-utils/src/main/scala/com/cds/utils/common/ConfigProvider.scala
@@ -0,0 +1,57 @@
+package com.cds.utils.common
+
+import java.util.{Map => JMap}
+
+/**
+  * Created by chendongsheng5 on 2017/6/2.
+  */
+private[common] trait ConfigProvider {
+
+  def get(key: String): Option[String]
+
+}
+
+private[common] class EnvProvider extends ConfigProvider {
+
+  override def get(key: String): Option[String] = sys.env.get(key)
+
+}
+
+private[common] class SystemProvider extends ConfigProvider {
+
+  override def get(key: String): Option[String] = sys.props.get(key)
+
+}
+
+private[common] class MapProvider(conf: JMap[String, String]) extends ConfigProvider {
+
+  override def get(key: String): Option[String] = Option(conf.get(key))
+
+}
+
+/**
+  * A config provider that only reads Spark config keys, and considers default values for known
+  * configs when fetching configuration values.
+  */
+private[common] class SparkConfigProvider(conf: JMap[String, String]) extends ConfigProvider {
+
+  import ConfigEntry._
+
+  override def get(key: String): Option[String] = {
+    if (key.startsWith("spark.")) {
+      Option(conf.get(key)).orElse(defaultValueString(key))
+    } else {
+      None
+    }
+  }
+
+  private def defaultValueString(key: String): Option[String] = {
+    findEntry(key) match {
+      case e: ConfigEntryWithDefault[_] => Option(e.defaultValueString)
+      case e: ConfigEntryWithDefaultString[_] => Option(e.defaultValueString)
+      case e: FallbackConfigEntry[_] => get(e.fallback.key)
+      case _ => None
+    }
+  }
+
+}
