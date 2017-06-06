e8782bf2c2a461525bc119e5479d7499c0be7dfe
anancds <anancds@163.com>
2017-06-06 11:18:41 +0800

    Updated config

--- scala-utils/src/main/scala/com/cds/utils/common/config/ConfigProvider.scala
+++ scala-utils/src/main/scala/com/cds/utils/common/config/ConfigProvider.scala
@@ -5,25 +5,25 @@ import java.util.{Map => JMap}
 /**
   * Created by chendongsheng5 on 2017/6/5.
   */
-private[spark] trait ConfigProvider {
+private[common] trait ConfigProvider {
 
   def get(key: String): Option[String]
 
 }
 
-private[spark] class EnvProvider extends ConfigProvider {
+private[common] class EnvProvider extends ConfigProvider {
 
   override def get(key: String): Option[String] = sys.env.get(key)
 
 }
 
-private[spark] class SystemProvider extends ConfigProvider {
+private[common] class SystemProvider extends ConfigProvider {
 
   override def get(key: String): Option[String] = sys.props.get(key)
 
 }
 
-private[spark] class MapProvider(conf: JMap[String, String]) extends ConfigProvider {
+private[common] class MapProvider(conf: JMap[String, String]) extends ConfigProvider {
 
   override def get(key: String): Option[String] = Option(conf.get(key))
 
@@ -33,7 +33,7 @@ private[spark] class MapProvider(conf: JMap[String, String]) extends ConfigProvi
   * A config provider that only reads Spark config keys, and considers default values for known
   * configs when fetching configuration values.
   */
-private[spark] class SparkConfigProvider(conf: JMap[String, String]) extends ConfigProvider {
+private[common] class SparkConfigProvider(conf: JMap[String, String]) extends ConfigProvider {
 
   import ConfigEntry._
 
